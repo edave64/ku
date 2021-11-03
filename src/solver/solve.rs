6e43ff81dca21ef2ba4374b2cfa0a8a558528db2
EDave64 <github5815@xcvbnm.org>
2021-11-03 23:01:11 +0100

    Improved backtracker readabilty

--- src/solver/solve.rs
+++ src/solver/solve.rs
@@ -9,26 +9,24 @@ pub fn solve(board: Board, prove_unique: bool) -> Result<Option<Board>, NonUniqu
         None => return Ok(Some(board)),
         Some((next_cell, possibilities)) => {
             let mut already_found = None;
-            for i in 0..9 {
-                if possibilities.mask & number_to_mask(i) == 0 { continue; }
+            for i in (0..9).filter(|&i| possibilities.mask & number_to_mask(i) > 0) {
                 let mut new_board = board.clone();
                 let marking = new_board.mark(next_cell, i);
                 if marking.is_err() { continue; }
 
-                let sub_ret = solve(new_board, prove_unique);
+                let sub_ret = solve(new_board, prove_unique)?;
 
-                if let Ok(Some(solved_board)) = sub_ret {
-                    if !prove_unique {
-                        return Ok(Some(solved_board));
-                    } else if already_found == None {
-                        already_found = Some(solved_board);
-                    } else {
-                        return Err(NonUniqueError {});
+                match sub_ret {
+                    Some(solved_board) => {
+                        if !prove_unique {
+                            return Ok(Some(solved_board));
+                        } else if already_found == None {
+                            already_found = Some(solved_board);
+                        } else {
+                            return Err(NonUniqueError {});
+                        }
                     }
-                } else if let Ok(None) = sub_ret {
-                    continue;
-                } else if let Err(err) = sub_ret {
-                    return Err(err);
+                    None => continue,
                 }
             }
             if let Some(board) = already_found {
